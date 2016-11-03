fb02531627994226763ea4fcbe4b9d33e7ea12cd
Pavel <pp-1995@mail.ru>
2016-11-03 21:10:00 +0500

    Исправил замечания

--- robbery.js
+++ robbery.js
@@ -29,21 +29,22 @@ exports.isStar = true;
  */
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     var bankTimezone = getTimezone(workingHours.from);
-    var timeIntervals = parseShedule(schedule).map(function (interval) {
-        return toBankTimezone(interval, bankTimezone);
-    });
-    timeIntervals = timeIntervals.concat(getBankNotWorkingTime(workingHours));
-    timeIntervals = timeIntervals.concat([
-        {
-            from: BEGIN_MONDAY - MS_IN_MIN,
-            to: BEGIN_MONDAY - MS_IN_MIN
-        },
-        {
-            from: END_WEDNESDAY + MS_IN_MIN,
-            to: END_WEDNESDAY + MS_IN_MIN
-        }
-    ]);
+    var timeIntervals = parseShedule(schedule, bankTimezone);
+    timeIntervals = timeIntervals.concat(
+        getBankNotWorkingTime(workingHours),
+        [
+            {
+                from: BEGIN_MONDAY - MS_IN_MIN,
+                to: BEGIN_MONDAY - MS_IN_MIN
+            },
+            {
+                from: END_WEDNESDAY + MS_IN_MIN,
+                to: END_WEDNESDAY + MS_IN_MIN
+            }
+        ]
+    );
     var foundMoments = getRobberyMoments(timeIntervals, duration);
+    var indexLastMomet = 0;
     var lastMoment = foundMoments[0];
 
     return {
@@ -53,11 +54,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            if (!lastMoment) {
-                return false;
-            }
-
-            return true;
+            return Boolean(lastMoment);
         },
 
         /**
@@ -72,16 +69,16 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
                 return '';
             }
             var day = new Date(lastMoment).getDate();
-            var hours = ('0' + new Date(lastMoment).getHours()).slice(-2);
-            var minutes = ('0' + new Date(lastMoment).getMinutes()).slice(-2);
+            var hours = toTimeFormat(new Date(lastMoment).getHours());
+            var minutes = toTimeFormat(new Date(lastMoment).getMinutes());
             var textDay = Object.keys(WEEK_DAYS).filter(function (weekDay) {
                 return WEEK_DAYS[weekDay] === day;
             })[0];
 
             return template
-                .replace(/(%DD)/, textDay)
-                .replace(/(%HH)/, hours)
-                .replace(/(%MM)/, minutes);
+                .replace('%DD', textDay)
+                .replace('%HH', hours)
+                .replace('%MM', minutes);
         },
 
         /**
@@ -90,44 +87,46 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            foundMoments.splice(0, 1);
-            if (foundMoments[0]) {
-                lastMoment = foundMoments[0];
-            }
-            if (!foundMoments[0]) {
+            indexLastMomet++;
+            if (!foundMoments[indexLastMomet]) {
                 return false;
             }
+            lastMoment = foundMoments[indexLastMomet];
 
             return true;
         }
     };
 };
-function toBankTimezone(interval, bankTimezone) {
-    var deltaZone = bankTimezone - interval.timezone;
+function toTimeFormat(number) {
+    return ('0' + number).slice(-2);
+}
+function toBaseTimezone(interval, baseTimezone) {
+    var deltaZone = baseTimezone - interval.timezone;
 
     return {
         from: interval.from + deltaZone * MS_IN_HOUR,
         to: interval.to + deltaZone * MS_IN_HOUR
     };
 }
-function parseShedule(schedule) {
-    var intervals = [];
-    Object.keys(schedule).forEach(function (name) {
-        schedule[name].forEach(function (obj) {
-            intervals.push(
-                {
-                    from: toDate(obj.from) + MS_IN_MIN,
-                    to: toDate(obj.to) - MS_IN_MIN,
-                    timezone: getTimezone(obj.from)
-                }
-            );
+function parseShedule(schedule, baseTimezone) {
+    return Object.keys(schedule).reduce(function (acc, name) {
+        var friendShedule = schedule[name].map(function (obj) {
+            return {
+                from: toDate(obj.from) + MS_IN_MIN,
+                to: toDate(obj.to) - MS_IN_MIN,
+                timezone: getTimezone(obj.from)
+            };
         });
-    });
+        acc = acc.concat(friendShedule);
 
-    return intervals;
+        return acc;
+    }, [])
+    .map(function (interval) {
+        return toBaseTimezone(interval, baseTimezone);
+    });
 }
 function toDate(timeString) {
-    var parsedTime = timeString.match(/^([А-Я]{2})\s(\d{2}):(\d{2})/);
+    var parsedTime = timeString.match(/^(ПН|ВТ|СР|ЧТ|ПТ|СБ|ВС)\s(\d{2}):(\d{2})/);
     var day = WEEK_DAYS[parsedTime[1]];
     var hours = parseInt(parsedTime[2], 10);
     var minutes = parseInt(parsedTime[3], 10);
@@ -135,20 +134,20 @@ function toDate(timeString) {
     return new Date(TODAY.getFullYear(), TODAY.getMonth(), day, hours, minutes).getTime();
 }
 function getTimezone(timeString) {
-    return parseInt(timeString.match(/\+(\d{1,2})$/)[1], 10);
+    return parseInt(parseInt(timeString.split('+')[1]), 10);
 }
-function isLessThan(duration, secondInterval) {
-    if (duration.to < secondInterval.from && duration.to <= END_WEDNESDAY &&
-        duration.from >= BEGIN_MONDAY && isCommonDay(duration.to, duration.from)) {
+function isLessThan(firstInterval, secondInterval) {
+    if (firstInterval.to < secondInterval.from && firstInterval.to <= END_WEDNESDAY &&
+        firstInterval.from >= BEGIN_MONDAY && isSameDay(firstInterval.to, firstInterval.from)) {
         return true;
     }
 
     return false;
 }
-function getDurationInterval(endInterval, duration) {
+function getDurationInterval(from, duration) {
     return {
-        from: endInterval + MS_IN_MIN,
-        to: endInterval + (duration + 1) * MS_IN_MIN
+        from: from + MS_IN_MIN,
+        to: from + (duration + 1) * MS_IN_MIN
     };
 }
 function getRobberyMoments(intervals, duration) {
@@ -157,17 +156,17 @@ function getRobberyMoments(intervals, duration) {
     for (var i = 0; i < intervals.length - 1; i++) {
         var firstInterval = intervals[i];
         var secondInterval = intervals[i + 1];
-        var durationInterval = getDurationInterval(firstInterval.to, duration);
-        while (isLessThan(durationInterval, secondInterval)) {
-            moments.push(durationInterval.from);
-            var nextRightLimit = durationInterval.from + (TIME_INDENT - 1) * MS_IN_MIN;
-            durationInterval = getDurationInterval(nextRightLimit, duration);
+        var candidateInterval = getDurationInterval(firstInterval.to, duration);
+        while (isLessThan(candidateInterval, secondInterval)) {
+            moments.push(candidateInterval.from);
+            var nextRightLimit = candidateInterval.from + (TIME_INDENT - 1) * MS_IN_MIN;
+            candidateInterval = getDurationInterval(nextRightLimit, duration);
         }
     }
 
     return moments;
 }
-function isCommonDay(firstDate, secondDate) {
+function isSameDay(firstDate, secondDate) {
     return new Date(firstDate).getDate() === new Date(secondDate).getDate();
 }
 function isInLimit(interval) {
